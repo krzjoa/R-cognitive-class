47368475b1be9719e712faf4b5a1e37e4a15a2c3
Krzysztof Joachimiak <kjoa.92@gmail.com>
2019-12-07 01:33:56 +0100

    path

--- path/path.R
+++ path/path.R
@@ -20,6 +20,26 @@ print.path_elem <- function(x, ...){
   x
 }
 
+`$.path_elem` <- function(a, b){
+  
+  if(length(a[[b]]) == 1 || b == "."){
+    raw.string <- deparse(substitute(a))
+    splitted.path <- strsplit(raw.string, "\\$")[[1]]
+    root.object <- get(splitted.path[1], parent.frame())
+    
+    splitted.path <- splitted.path[-1]
+    elem.to.be.removed <- grepl("`.*`", splitted.path)
+    splitted.path[elem.to.be.removed] <- substr(splitted.path[elem.to.be.removed], 2,
+                                                nchar(splitted.path[elem.to.be.removed]))
+    path <- paste(splitted.path, collapse = .Platform$file.sep)
+    last.node <- attr(a[[b]], 'node')
+    last.node <- if(is.null(last.node)) "" else last.node
+    file.path(attr(root.object, 'node'), path, last.node)
+  } else {
+    a[[b]]
+  }
+}
+
 a1 <- path_elem("fileA.RData")
 a2 <- path_elem("fileB.RData")
 a3 <- path_elem("data", list(a1, a2))
