147cc44e40a7e3064bb9d11dcc2db2dbf9570320
Krzysztof Joachimiak <kjoa.92@gmail.com>
2019-12-07 02:48:51 +0100

    from config

--- path/path.R
+++ path/path.R
@@ -1,13 +1,17 @@
 path_elem <- function(node = NULL, children = NULL, ...){
   
-  if(length(node) > 1){
-    node <- node[1]
-    children <- node[-1]
-  }
+  # if(length(node) > 1){
+  #   node <- node[1]
+  #   children <- node[-1]
+  # }
+  
+  if(is.null(names(children)) & !is.null(children))
+    children <- setNames(children, sapply(children, function(x) attr(x, 'node')))
+  nms <- names(children)
   
   path.elem <- structure(list(), class = "path_elem")
   attr(path.elem, 'node') <- node 
-  for(i in children){path.elem[[attr(i, 'node')]] <- i}
+  for(i in seq_along(children)){path.elem[[nms[[i]]]] <- children[[i]]}
   path.elem['.'] <- node
   return(path.elem)
 }
@@ -31,18 +35,53 @@ print.path_elem <- function(x, ...){
     elem.to.be.removed <- grepl("`.*`", splitted.path)
     splitted.path[elem.to.be.removed] <- substr(splitted.path[elem.to.be.removed], 2,
                                                 nchar(splitted.path[elem.to.be.removed]))
-    path <- paste(splitted.path, collapse = .Platform$file.sep)
-    last.node <- attr(a[[b]], 'node')
-    last.node <- if(is.null(last.node)) "" else last.node
-    file.path(attr(root.object, 'node'), path, last.node)
+    
+    splitted.path <- c(splitted.path, b)
+    
+    fun <- function(x, y){
+      obj <- x[[1]]
+      last.node <- attr(obj[[y]], 'node')
+      last.node <- if(is.null(last.node)) "" else last.node
+      list(obj[[y]], file.path(x[[2]], last.node))
+    }
+    
+    Reduce(fun, splitted.path, list(root.object, attr(root.object, 'node')))[[2]]
   } else {
     a[[b]]
   }
 }
 
-a1 <- path_elem("fileA.RData")
-a2 <- path_elem("fileB.RData")
-a3 <- path_elem("data", list(a1, a2))
-a4 <- path_elem("files", list(a3))
+cnf <- config::get(config = 'default', file = 'conf.yml')
 
-a4$data$fileA.RData
+file.section <- cnf$kFiles
+
+dir_structure <- function(config.file.section, root.key = 'kRoot'){
+  print(length(config.file.section))
+  if(length(config.file.section) > 1){
+    node <- config.file.section[[root.key]]
+    children <- config.file.section[which(names(config.file.section) != root.key)]
+    path_elem(node, sapply(children, dir_structure))
+  } else {
+    path_elem(node = config.file.section[1])
+  }
+}
+
+cnfs <- dir_structure(file.section)
+
+cnfs$kNestedData$kFile3..
+
+
+file.section <- cnf$kFiles
+
+file.section
+
+
+# a1 <- path_elem("fileA.RData")
+# a2 <- path_elem("fileB.RData")
+# a3 <- path_elem("data", list(A1 = a1, A2 = a2))
+# a4 <- path_elem("files", list(a3))
+# 
+# a4$data$A1
+# a4$data$.
+
+
