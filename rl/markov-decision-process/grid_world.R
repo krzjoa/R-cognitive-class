bb20cc09c835a526dba5c06f3d3faba09c4a5557
Krzysztof Joachimiak <kjoa.92@gmail.com>
2019-12-30 13:06:16 +0100

    RL

--- rl/markov-decision-process/grid_world.R
+++ rl/markov-decision-process/grid_world.R
@@ -1,7 +1,6 @@
 library(R6)
 library(matricks)
 
-
 grid_world <- R6Class(
   classname = 'grid_world',
   
@@ -83,7 +82,6 @@ grid_world <- R6Class(
 standard_grid <- function(){
   gw <- grid_world$new(6, 5, c(1, 1))
   
-  # Actions
   m(T, T, T, F, T|
     T, T, F, T, T|
     F, T, T, T, F|
@@ -92,16 +90,15 @@ standard_grid <- function(){
     F, T, T, T, F) -> actions
   
   rewards <- matrix(0, 6, 5)
+  rewards <- set_values(rewards, 
+                        c(4, 4) ~ 1.,
+                        c(5, 3) ~ -1)  
   
-  set_values(rewards, 
-             c(4, 4) ~ -1,
-             c(5, 3) ~ 1) -> rewards
-  
-  gw$set(rewards = rewards,
-         actions = actions)
-  
+  gw$set(actions = actions,
+         rewards = rewards)
   gw
 }
+
 
 negative_grid <- function(step.cost = -0.1){
   gw <- standard_grid()
@@ -110,126 +107,5 @@ negative_grid <- function(step.cost = -0.1){
   gw$rewards <- rewards
   gw
 }
-
-# gw <- negative_grid()
-# 
-# gw$move('R')
-# 
-# 
-# gw$is_action_possible('R')
-
-
-conv.threshold <- 10e-4
-
-#' @name matrix_idx
-#' @title Return all the matrix/data.frame indices
-#' @param x matrix or data.frame
-#' @param mask logical matrix
-#' @return list of two-element vectors, where values describe (x, y) position respecitively
-#' @example 
-#' mat <- matrix(0, 3, 3)
-#' matrix_idx(mat)
-matrix_idx <- function(x, mask = NULL){
-  n.col <- ncol(x)
-  n.row <- nrow(x)
-  out <- expand.grid(1:n.col, 1:n.row)
-  
-  # browser()
-  
-  out2 <- asplit(out, 1)
-  out2 <- Map(as.vector, out2)
-  
-  if (is.matrix(mask)){
-    out3 <- matrix(out2, nrow = n.row, ncol = n.col)
-    return(out3[mask])
-  } else {
-    return(out2)
-  }
-}
-
-#' @name neighbour_idx
-#' @title Get all indices in neighbourhood 
-#' @param mat matrix or data.frame
-#' @param idx two-element vector
-#' @param mask logical matrix; optional
-#' @param diagonal include diagonal neighbours
-#' @param include.idx include current index
-#' @example 
-#' mat <- matrix(0, 3, 3)
-#' neighbour_idx(mat, c(1, 2))
-#' neighbour_idx(mat, c(1, 2), diagonal = FALSE)
-#' neighbour_idx(mat, c(1, 2), diagonal = FALSE, include.idx = TRUE)
-#' # With mask
-#' mat <- matrix(0, 3, 4)
-#' mask <- m(F, F, T, T | F, F, F, F | T, T, F, T)
-#' neighbour_idx(mat, c(1, 2), mask = mask)
-neighbour_idx <- function(mat, idx, mask = NULL, diagonal = TRUE, include.idx = FALSE){
-  n.row <- nrow(mat)
-  n.col <- ncol(mat)
-  nidx <- NULL
-  min.row <- max(1, idx[1]-1)
-  max.row <- min(n.row, idx[1] + 1)
-  min.col <- max(1, idx[2] - 1)
-  max.col <- min(n.col, idx[2] + 1)
-  
-  for (i in min.row:max.row) {
-    for (j in min.col:max.col) {
-      
-      if (!include.idx & idx[1] == i & idx[2] == j)
-        next
-      
-      if (!diagonal & (abs(idx[1]-i) + abs(idx[2] - j)) == 2)
-        next
-      
-      if (!is.null(mask)) {
-        if (mask[i, j])
-          nidx <- c(nidx, list(c(i, j)))
-      } else {
-        nidx <- c(nidx, list(c(i, j)))
-      }
-    }
-  }
-  nidx
-}
-
-
-
-main <- function(){
-  grid <- standard_grid()
-  
-  V <- matrix(0, nrow = grid$height, ncol = grid$width)
-  gamma <- 1.0 # discount factor
-  states <- matrix_idx(grid$actions, grid$actions)
-  mask <- grid$actions & (grid$rewards == 0)
-  # non.terminal.states <- matrix_idx(grid$actions, )
-  
-  while (TRUE) {
-    biggest.change <- 0
-    
-    for (s in states){
-      old.v <- V[s[1], s[2]]
-      
-      # V(s) only has value if `s` it's not a terminal state
-      possible.transitions <- neighbour_idx(grid$actions, 
-                                            idx = s, mask = mask, 
-                                            diagonal = FALSE)
-      for (ns in non.terminal.states){
-        
-        new.v <- 0
-        p.a <- 1.0 / length(possible.transitions)
-        
-        
-        
-        
-      }
-      
-    }
-        
-    
-  }
-  
-  
-}
-  
 
 
