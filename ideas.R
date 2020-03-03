720a2c6ac6fb89201c1684bd1ebc0762663bd6be
Krzysztof Joachimiak <joachimiak.krzysztof@gmail.com>
2020-03-03 15:05:00 +0100

    Update ideas.R

--- ideas.R
+++ ideas.R
@@ -4,3 +4,8 @@ tsunami::add_lags(PRICE ~ lags(1,2,3,4) + fill_na(na.locf),
 add_lags.data.frame
 add_lags.data.table
 add_lags.tsibble
+
+# looplyr
+cars %>% 
+  looplyr::loop_mutate(loop_cols(), {mx <- max(Revenue); glue("col_{mx}")} := {.x})
+  looplyr::loop_summarise(loop_cols(), {} := {.x})
