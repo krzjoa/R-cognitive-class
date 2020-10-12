ec1c1a1ce00c0fcf35d34498cbfd31e56b9c8923
Krzysztof Joachimiak <kjoa.92@gmail.com>
2020-10-13 00:09:58 +0200

    cnn

--- dlr/R/visualize.R
+++ dlr/R/visualize.R
@@ -1,9 +1,8 @@
 #' @name adjacency_matrix
 #' @title Creat adjacency matrix for all the operations in the context
 #' @param ctx dlr_context object
-#' @useDynLib dlr C_adjacency_matrix
 #' @examples
-#' library(dlr)
+#' library(aria)
 #' ctx <- get_context()
 #' register_ops(ctx, cars)
 #' register_ops(ctx, data.frame)
@@ -17,7 +16,6 @@
 #' @export
 adjacency_matrix <- function(ctx){
   mat <- .Call(C_adjacency_matrix, ctx[['_container']])
-  # add_class(mat) <- "soprano_adjacency_matrix"
   class(mat) <- c("soprano_adjacency_matrix", class(mat))
   return(mat)
 }
